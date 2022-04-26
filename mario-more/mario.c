04bec42d283c366b2356fad4ae1c6058ff391c73
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-26 15:09:39 +0000

    Tue, Apr 26, 2022, 3:09 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,9 +3,13 @@
 
 int main(void)
 {
+    int size =size();
 
 }
 
 int size(void){
-    
+    int size = get_int("input a number 1-8");
+    if(size>0&&size<9) return size;
+    printf("not valid input");
+    size();
 }
