9416568e479b65ebc6f8d25a5853dab609c99396
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-26 16:57:56 +0000

    Tue, Apr 26, 2022, 4:57 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,13 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 int size (void);
+string blocks (int i);
 int main(void)
 {
     int high= size();
     printf("%i",high);
 
 }
-
+// Gets the size of the "blocks"
 int size(void){
     int stacks = get_int("input a number 1-8: \n");
     if(stacks>0&&stacks<9) return stacks;
