966b4b6ebabeb165fa68120143bccfc5aef72353
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-26 16:49:30 +0000

    Tue, Apr 26, 2022, 4:49 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,7 +9,7 @@ int main(void)
 }
 
 int size(void){
-    int size = get_int("input a number 1-8");
+    int dicks = get_int("input a number 1-8");
     if(size>0&&size<9) return size;
     printf("not valid input");
     size();
