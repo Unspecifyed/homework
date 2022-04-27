bdd285a9e4a99acd72376dcbeef65521d256988c
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-27 20:13:26 +0000

    Wed, Apr 27, 2022, 8:13 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -24,7 +24,7 @@ int size(void)
 //creates whitespace to allign the center
 void whiteSpace(int iteration, int total){
     iteration=iteration-1;
-    if(iteration==0) return;
+    if(iteration<0) return;
     printf(" ");
     whiteSpace(iteration, total);
     return;
