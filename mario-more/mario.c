e78dc059601dbed866141c2b0dac139590fd6b97
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-27 22:22:25 +0000

    Wed, Apr 27, 2022, 10:22 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,10 +1,29 @@
-// Prints an n-by-n grid of bricks with a loop
-
 #include <cs50.h>
 #include <stdio.h>
-void edge(int size, char c);
+int size (void);
+void edge (int iteration, char c);
 int main(void)
 {
-    int size = get_int("Size: ");
-
+    int high= size();// how high the blocks are
+    stacks(0,high);
+    return 0;
 }
+// Gets the size of the "blocks"
+int size(void)
+{
+    int stacks = get_int("input a number 1-8: \n");
+    if(stacks>0&&stacks<9) return stacks;
+    printf("not valid input\n");
+    size();
+    return stacks;
+}
+// Creates the edge of the blocks
+void edge (int iteration, char c)
+{
+    iteration=iteration-1;
+    printf(c);
+    if(iteration==0) return;
+    edge(iteration);
+    return;
+}
+
