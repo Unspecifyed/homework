252ff8b8149e9b930b453018940ce03167aae5e8
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-27 20:05:31 +0000

    Wed, Apr 27, 2022, 8:05 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -23,7 +23,7 @@ int size(void)
 
 //creates whitespace to allign the center
 void whiteSpace(int iteration, int total){
-    iteration=iteration+1;
+    iteration
     if(total-iteration==0) return;
     printf(" ");
     whiteSpace(iteration, total);
@@ -46,7 +46,7 @@ void edge (int iteration)
 void stacks(int iteration, int total)
 {
     iteration = iteration+1;
-    whiteSpace(0, total);
+    whiteSpace(iteration, total);
     edge(iteration);// left side of blocks
     printf("  "); // space in the middle
     edge(iteration); //rightside
