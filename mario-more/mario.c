b9dbfdebeaf4ea45704cb6b585e898fa23a9da64
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-27 21:53:28 +0000

    Wed, Apr 27, 2022, 9:53 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,60 +1,22 @@
+// Prints an n-by-n grid of bricks with a loop
+
 #include <cs50.h>
 #include <stdio.h>
-int size (void);
-// void whiteSpace(int iteration, int total);
-void edge (int iteration, char c);
-void stacks(int iteration, int total);
+
 int main(void)
 {
-    int high= size();// how high the blocks are
-    stacks(0,high);
-    return 0;
-
-}
-// Gets the size of the "blocks"
-int size(void)
-{
-    int stacks = get_int("input a number 1-8: \n");
-    if(stacks>0&&stacks<9) return stacks;
-    printf("not valid input\n");
-    size();
-    return stacks;
-}
-
-//creates whitespace to allign the center
-//void whiteSpace(int iteration, int total){
- //   iteration=iteration-1;
-  //  if(iteration==0)return;
-   // printf(" ");
-   // whiteSpace(iteration, total);
-   // return;
-
-
-//}
-
-// Creates the edge of the blocks
-void edge (int iteration, char c)
-{
-    iteration=iteration-1;
-    printf(c);
-    if(iteration==0) return;
-    edge(iteration);
-    return;
-}
-
-//stacks the blocks
-void stacks(int iteration, int total)
-{
-    // whiteSpace(iteration, total);
-    iteration = iteration+1;
-    edge(iteration," ");// left side of blocks
-    edge(iteration,"#");// left side of blocks
-    printf("  "); // space in the middle
-    edge(iteration,"#"); //rightside
- //   whiteSpace(iteration, total);
-    printf("\n");
-  //  whiteSpace(iteration, total);
-    if(iteration==total) return;
-    stacks(iteration, total);
-    return;
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
