b1a4a84e7aca0f76e4133826ca22cbd034c3d02e
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-28 18:38:35 +0000

    Thu, Apr 28, 2022, 6:38 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 int credit_number_digits(long n);
+int last_two(int n);
 int main(void)
 {
     const long CREDIT_NUMBER = get_long("Number: ");
     const int CREDIT_DIGIT = credit_number_digits(CREDIT_NUMBER);
+    const int CREDIT_LAST_TWO = last_two(CREDIT_NUMBER);
     return 0;
 }
 // Finds the number of digits
@@ -15,4 +17,15 @@ int credit_number_digits(long n)
     digit = digit + 1;
     printf("%d", digit);
     return digit ;
+}
+// Finds the last two numbers
+int last_two(int n)
+{
+    if(n < 100)
+    {
+        return n;
+    }
+    n = n / 10;
+    last_two(n);
+    return 0;
 }
