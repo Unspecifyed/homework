c380cfc6bb95f2c47d98f6b11411231af5e3f7cd
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-28 17:11:46 +0000

    Thu, Apr 28, 2022, 5:11 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,11 +5,13 @@ int credit_number_digits(long n);
 int main(void)
 {
     const long CREDIT_NUMBER= get_long("Number: ");
+    credit_number_digits(CREDIT_NUMBER);
     return 0;
 }
 // Finds the number of digits
 int credit_number_digits(long n)
 {
     int digit = (int) log10(n);
-    
+    printf("%d", digit);
+    return digit;
 }
