89d1f725a2ec355b9f2d0a5a6a756a21b747f51e
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-28 17:08:30 +0000

    Thu, Apr 28, 2022, 5:08 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 int credit_number_digits(long n);
 int main(void)
 {
@@ -9,5 +10,6 @@ int main(void)
 // Finds the number of digits
 int credit_number_digits(long n)
 {
+    int digit = (int) log10(n);
     
 }
