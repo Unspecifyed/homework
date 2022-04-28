a234f58fd044b0f6d48da0ce9526aaac6a8d775e
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-28 17:03:01 +0000

    Thu, Apr 28, 2022, 5:03 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-
+int credit_number_digits(long n);
 int main(void)
 {
     const long CREDIT_NUMBER= get_long("Number: ");
     return 0;
+}
+// Finds the number of digits
+int credit_number_digits(long n)
+{
+    
 }
