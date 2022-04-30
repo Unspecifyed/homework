676831d30c9f7e66a0023992d580abc3c59a4bbc
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-04-30 20:30:09 +0000

    Sat, Apr 30, 2022, 8:30 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -68,7 +68,7 @@ int brand(int digit, int last)
 // Gets the nth diget of the number inputed
 int getNthDigit(int num, int n)
 {
-    int result = num / 10**n % 10;
+    int result = num / pow(10,n) % 10;
     return result;
 }
 // check sum of the number given the number, nth element, amount of digets
