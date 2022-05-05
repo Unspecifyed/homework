845699e39c13b72b06f8d85203997fbd7816637a
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-05-05 20:47:42 +0000

    Thu, May 5, 2022, 8:47 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 
 void scramb(string input);
+bool checkKey(string key);
 int main(int argc, string argv[])
 {
-    int error = 0;
     string word1 = get_string("plaintext: ");
     int argumentCount = argc;
     string key = argv[1];
+    bool error = checkKey(string key);
 
     printf("ciphertext: ");
-    return error;
+    return 0;
 }
 void scramb(string input)
 {
