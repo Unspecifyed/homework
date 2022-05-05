3272206182bf87f7d71c4d6ed43bf24ceb016f5c
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-05-05 16:24:04 +0000

    Thu, May 5, 2022, 4:24 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,5 +5,6 @@ int main(int argc, string argv[])
 {
     int cipher[] = {'J','T','R','E','K','Y','A','V','O','G','D','X','P','S','N','C','U','I','Z','L','F','B','M','W','H','Q'};
     string word1 = get_string("plaintext: ");
+    
     printf("ciphertext: ");
 }
