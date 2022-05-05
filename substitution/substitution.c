712a773c40f54ddef135e44770eb1df6ad7702bd
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-05-05 22:51:01 +0000

    Thu, May 5, 2022, 10:51 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -33,7 +33,7 @@ int checkKey(string key, int aC)
     int n = 0;
     int valid = 0;
     int value = -1;
-    if(ac != 2)
+    if(aC != 2)
     {
         return 4;
     }
@@ -47,7 +47,7 @@ int checkKey(string key, int aC)
         value = (int)key[n];
         if(value < 65)
         {
-            valid = 1 // invalid char
+            valid = 1; // invalid char
             break;
         }
         if(value > 90 && value < 97)
