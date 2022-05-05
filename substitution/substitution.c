e43ffa6ca38504f3f6245ad197856caca6d41b20
Francis Cottrell-Eshaghi <39974788+Unspecifyed@users.noreply.github.com>
2022-05-05 16:29:07 +0000

    Thu, May 5, 2022, 4:29 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,10 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+char scramb(string input);
 int main(int argc, string argv[])
 {
-    int cipher[] = {'J','T','R','E','K','Y','A','V','O','G','D','X','P','S','N','C','U','I','Z','L','F','B','M','W','H','Q'};
+    char cipher[] = {'J','T','R','E','K','Y','A','V','O','G','D','X','P','S','N','C','U','I','Z','L','F','B','M','W','H','Q'};
     string word1 = get_string("plaintext: ");
-    
+
     printf("ciphertext: ");
+}
+char scramb(string input)
+{
+    
 }
