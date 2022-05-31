e1e7b2a79140eb88cb36a43ab66a40334464604e
Unspecifyed <39974788+Unspecifyed@users.noreply.github.com>
2022-05-31 13:01:51 -0400

    allthis

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,80 +4,129 @@
 bool valid(int count, string key);
 bool caps(char c);
 void cyper(string key, string ptext);
-int main(int argc, string argv[]) {
+char lowerChar(char c);
+char upperChar(char c);
+bool isSpace(char c);
+int main(int argc, string argv[])
+{
   const int ARGUMENTCOUNT = argc;
   const string KEY = argv[1];
   const bool VALIDKEY = valid(ARGUMENTCOUNT, KEY);
 
-  if (!VALIDKEY) {
+  if (!VALIDKEY)
+  {
     return 1;
   }
-
   const string PLAINTEXT = get_string("plaintext: ");
+  printf("ciphertext: ");
   cyper(KEY, PLAINTEXT);
 
   return 0;
 }
 
-bool valid(int count, string key) {
+bool valid(int count, string key)
+{
   bool isValid = true;
-  if (key == NULL) {
+  if (key == NULL)
+  {
     printf("Usage: ./substitution key");
     return false;
   }
   int length = strlen(key);
-  if (count != 2) {
+  if (count != 2)
+  {
     printf("Usage: ./substitution key");
     isValid = false;
   }
 
-  if (length != 26) {
+  if (length != 26)
+  {
     printf("Key must contain 26 characters.");
     isValid = false;
   }
-  if (isValid) {
-    int n = 0;
-    int nSub = 0;
-    while (n < 26) {
-      char a = key[n];
-      if (a == ' ') {
-      } else {
-        while (nSub < 26) {
-          char b = key[nSub];
-          if (a == b) {
-            return false;
-          }
-          nSub++;
-        }
-      }
-      n++;
-    }
-  }
+
   return isValid;
 }
 
-bool caps(char c) {
+bool caps(char c)
+{
   bool isCaps = false;
-  if (c > 64 && c < 91) {
+  if (c > 64 && c < 91)
+  {
     isCaps = true;
   }
   return isCaps;
 }
 
-void cyper(string key, string ptext) {
+void cyper(string key, string ptext)
+{
   int i = 0;
   int cAmount = 65;
   int lAmount = 97;
-  int pValue = 0;
-  char out = 'a';
-  while (ptext[i] != '\0') {
-    if (caps(ptext[i])) {
-      pValue = ptext[i] - cAmount;
-    } else {
-      pValue = ptext[i] - lAmount;
+  char out = '0';
+  while (i <= strlen(ptext))
+  {
+    char plainChar = ptext[i];
+    bool plainCharCaps = caps(plainChar);
+    int plainCharValue = 0;
+    bool spaceChar = false;
+    if (plainChar == ' ')
+    {
+      plainCharValue = 0;
+      spaceChar = true;
     }
-    out = key[pValue];
-    printf("%c", out);
+    else if (plainCharCaps)
+    {
+      plainCharValue = plainChar - cAmount;
+    }
+    else
+    {
+      plainCharValue = plainChar - lAmount;
+    }
+    char cLetter = key[plainCharValue];
+    if (caps(cLetter) && !caps(plainChar))
+    {
+      out = lowerChar(cLetter);
+    }
+    else if (!caps(cLetter) && caps(plainChar))
+    {
+      out = upperChar(cLetter);
+    }
+    else
+    {
+      out = cLetter;
+    }
+
+    if (spaceChar)
+    {
+      printf(" ");
+    }
+    else
+    {
+      printf("%s", out);
+    }
+
     i++;
+  }
+}
+char lowerChar(char c)
+{
+  c = c + 32;
+  return c;
+}
+char upperChar(char c)
+{
+  c = c - 32;
+  return c;
+}
+bool isSpace(char c)
+{
+  if (c == ' ')
+  {
+    return true;
+  }
+  else
+  {
+    return false;
   }
 }
